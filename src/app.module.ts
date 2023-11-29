3d0f33b23dc1eb88e99c6e3c0a4118fd22bb533d
Anderson Soares <anderson.soares@defensoria.ro.def.br>
2023-11-29 09:54:46 -0400

    remove jest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
